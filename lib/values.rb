6ec302cee0d7e00efa9f456c175a20e7095a5587
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 18:54:06 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -8,6 +8,32 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
+def get_the_keys(groceries)
+  groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+  groceries.keys
+end
+
+def get_the_min(groceries)
+  groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+  groceries.values.flatten.min
+end
+
 def get_the_values(groceries)
-  #code your solution here!
+  groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+  groceries.values.flatten
 end
