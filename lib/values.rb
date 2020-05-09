de8e63b986a9b7d7dfc303bae4eae4bc25ee8ec2
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 18:46:22 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,11 +9,7 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  groceries = {
-  dairy: ["milk", "yogurt", "cheese"],
-  vegetable: ["carrots", "broccoli", "cucumbers"],
-  meat: ["chicken", "steak", "salmon"],
-  grains: ["rice", "pasta"]
+  groceries = {dairy: ["milk", "yogurt", "cheese"], vegetable: ["carrots", "broccoli", "cucumbers"], meat: ["chicken", "steak", "salmon"], grains: ["rice", "pasta"]
 }
   groceries.keys
 end
