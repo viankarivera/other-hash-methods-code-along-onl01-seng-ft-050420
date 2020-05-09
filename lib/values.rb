ef14d3ae40435a978164fe14d9e996e2408d65cb
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 18:50:02 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -19,6 +19,12 @@ def get_the_keys(groceries)
 end
 
 def get_the_min(groceries)
-
+  groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+groceries.values 
 
 end 
